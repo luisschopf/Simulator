e62625e89888b5e0587e636f389c95751d6d9c30
luisschopf <luisschopf47@gmail.com>
2026-09-18 12:08:24 +0200

    edr44rd

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,7 @@ public class App {
         Fighter orc = new Orc("Orci", 100);
         Fighter wiz = new Wizard("Wizi", 100);
         orc.fight(wiz);
+        orc.bringtInfos();
+        wiz.bringtInfos();
     }
 }
