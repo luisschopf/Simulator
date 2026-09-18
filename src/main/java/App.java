5422b36d768f6a4791c00115fe3c0ba19b53fe43
luisschopf <luisschopf47@gmail.com>
2026-09-18 12:06:11 +0200

    edr44rd

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,8 @@
 public class App {
 
     public static void main(String[] args) {
-        Fighter f = new Orc("Max", 100);
-        Fighter f2 = new Wizard("Susi", 100);
+        Fighter orc = new Orc("Orci", 100);
+        Fighter wiz = new Wizard("Wizi", 100);
+        orc.fight(wiz);
     }
 }
