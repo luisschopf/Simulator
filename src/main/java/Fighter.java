4b01ad40cf2a50e9d6e1428bb5dfc64dd452cab9
luisschopf <luisschopf47@gmail.com>
2026-09-18 12:12:49 +0200

    edr44rd

---
+++ src/main/java/Fighter.java
@@ -0,0 +1,32 @@
+public abstract class Fighter {
+    private String name;
+    private int health;
+
+    public Fighter(String name, int health){
+        this.name=name;
+        this.health=health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public void fight(Fighter enemy){
+
+        System.out.println(enemy.getName()+ " wird angegriffen");
+    }
+    public void bringtInfos(){
+        System.out.println(name+ " ,Health: "+health);
+    }
+}
